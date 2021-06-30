f1f92b37eb689a06e9142da55f10987e5c2b2a72
kykzk46 <mwckkwork@gmail.com>
2021-06-30 17:33:18 +0800

    Add a fast implimentation of ibm2ieee

--- ibm2ieee/ibm2ieee/main.cpp
+++ ibm2ieee/ibm2ieee/main.cpp
@@ -7,7 +7,47 @@ https://www.crewes.org/Documents/ResearchReports/2017/CRR201725.pdf
 *******************************************************************************/
 #include <cmath>
 
-float ibm2ieee(unsigned int ibm)
+float ibm2ieee_fast(unsigned int ibm)
+{
+	// copy sign
+	unsigned int buffer = (ibm & 0x80000000);
+
+	// fraction
+	int frac = (ibm << 8);
+	int shift = 0;
+	do
+	{
+		// According to IBM standard, the fraction part is normalized by shifting 
+		// 4bits at a time, until the leftmost digit is 0
+		// Meaning the first bit bust be 0, so a do-while is correct
+		frac = frac << 1;
+		shift++;
+	} while (frac & 0x80000000 != 1);
+
+	// Handle implicit 1
+	frac = frac << 1;
+	shift++;
+
+	frac = (frac >> 9) & 0x007FFFFF;
+	buffer = buffer | frac;
+
+	// exponent
+	char bias16 = 64;
+	char expn16 = (ibm & 0x7f000000) >> 24;
+	expn16 = expn16 - bias16;
+
+	char expn2 = 4 * expn16 - shift;
+	char bias2 = 127;
+	expn2 = expn2 + bias2;
+	unsigned int expn = expn2;
+	expn = (expn & 0x000000FF) << 23;
+
+	buffer = buffer | expn;
+	
+	return reinterpret_cast<float&>(buffer);
+}
+
+float ibm2ieee_intuitive(unsigned int ibm)
 {
 	char bias = 64;
 	char sign = (ibm & 0x80000000) >> 31;
@@ -38,7 +78,8 @@ unsigned int ieee2ibm(float ieee)
 	char sign = (buffer) >> 31;
 	buffer = buffer & 0x80000000;
 	
-	int intg = static_cast<int>(ieee) * sign;
+	int intg = static_cast<int>(ieee) * pow(-1, sign);
+
 
 	return 0;
 }
@@ -50,8 +91,8 @@ int main()
 	//
 	unsigned int ibm2ieeeTests[10];
 	// Case 1: Wiki example
-	// IEEE -118.625
-	// IBM 0xC276A000
+	// IEEE -118.625, 0xC2ED4000, 11000010 11101101 01000000 00000000
+	// IBM            0xC276A000, 11000010 01110110 10100000 00000000
 	ibm2ieeeTests[0] = 0xC276A000; 
 
 	// Case 2: 
@@ -60,7 +101,8 @@ int main()
 	ibm2ieeeTests[1] = 0x40280000;
 
 	unsigned int t = ibm2ieeeTests[0];
-	float f2 = ibm2ieee(t);
+	float f1 = ibm2ieee_intuitive(t);
+	float f2 = ibm2ieee_fast(t);
 
 	//
 	// Test ieee to ibm
@@ -69,8 +111,8 @@ int main()
 	ieee2ibmTests[0] = -118.625;
 	ieee2ibmTests[1] = 0.15625;
 
-	float f1 = reinterpret_cast<float&>(ieee2ibmTests[0]);
-	ieee2ibm(f1);
+	unsigned int f3 = reinterpret_cast<unsigned int&>(ieee2ibmTests[0]);
+	ieee2ibm(ieee2ibmTests[1]);
 
 	return 0;
 }
